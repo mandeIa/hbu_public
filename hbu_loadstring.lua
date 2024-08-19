29110c698957863be8cb8991012dc73ee253fa57
Connor Burnside <163656947+mandeIa@users.noreply.github.com>
2024-08-18 22:32:31 -0500

    Update hbu_loadstring.lua

--- hbu_loadstring.lua
+++ hbu_loadstring.lua
@@ -1,1 +1,2 @@
 script_key = getgenv().hbu.CoreSystems.MainOps.Framework.Authorization.License
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/179c1461abc9f61a916bc1452e2c9f7f.lua"))()
