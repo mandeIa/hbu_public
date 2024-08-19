4c4d69060d1f36208e187c779bfb20132dbfde44
Connor Burnside <163656947+mandeIa@users.noreply.github.com>
2024-08-19 15:06:39 -0500

    Update hbu_table.lua

--- hbu_table.lua
+++ hbu_table.lua
@@ -102,22 +102,26 @@ getgenv().hbu = {
         FOVControl = {
             Settings = {
                 ScopeMode = "Dynamic", -- Options: "Static", "Dynamic"
+                Color = Color3.new(0.741176, 0.631373, 0.145098),
                 Zones = {
                     DynamicFieldOfView = {Value = 100},
                     SilentScope = {
                         Visibility = true,
                         FillState = true,
-                        RadiusSize = {Value = 100}
+                        RadiusSize = {Value = 100},
+                        Color = Color3.fromRGB(37, 121, 189)
                     },
                     AimAssistScope = {
                         Visibility = false,
                         FillState = true,
-                        RadiusSize = {Value = 85}
+                        RadiusSize = {Value = 85},
+                        Color = Color3.fromRGB(255, 0, 0)
                     },
                     DynamicRadiusScope = {
                         Visibility = false,
                         FillState = false,
-                        RadiusSize = {Value = 100}
+                        RadiusSize = {Value = 100},
+                        Color = Color3.fromRGB(255, 255, 255)
                     }
                 }
             }
